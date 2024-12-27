64e752f93e5c7dfcf774a403ddbbf7d812d82e37
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2024-12-27 13:49:11 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 56 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int cnt = 0;
+
+int recursion(const char * s, int l, int r) {
+  cnt++;
+  if(l >= r) return 1;
+  else if(s[l] != s[r]) return 0;
+  else return recursion(s, l+1, r-1);
+}
+int isPalindrome(const char * s) {
+  return recursion(s, 0, strlen(s) - 1);
+}
+int main() {
+  int T;
+  string S;
+  cin >> T;
+  for(int i = 0; i < T; i++) {
+    cnt = 0;
+    cin >> S;
+    cout << isPalindrome(S.c_str()) << " " << cnt << endl;
+  }
+  return 0;
+}
