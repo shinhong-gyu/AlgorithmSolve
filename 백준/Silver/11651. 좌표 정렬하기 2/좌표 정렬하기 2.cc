75c5a17241dbc2dfe6fd50e70f0dec302112cc16
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2024-12-30 17:51:55 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 104 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Coordinate
+{
+	int x, y;
+};
+
+ostream& operator<<(ostream& os, const Coordinate& p)
+{
+	return os << p.x << " " << p.y << "\n";
+}
+
+bool compare(Coordinate p1, Coordinate p2)
+{
+	if (p1.y == p2.y)
+		return p1.x < p2.x;
+	return p1.y < p2.y;
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<Coordinate> points(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> points[i].x >> points[i].y;
+	}
+
+	sort(points.begin(), points.end(), compare);
+
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << points[i];
+	}
+}
+
