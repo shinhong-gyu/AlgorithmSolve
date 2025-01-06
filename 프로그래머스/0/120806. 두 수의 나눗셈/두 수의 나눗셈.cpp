59c6922e502a1cdc58e70d1faf524ab58846f857
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-01-06 14:04:01 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    float fnum1 = num1;
+    float fnum2 = num2;
+    float result = fnum1/fnum2*1000;
+    return result;
+}
