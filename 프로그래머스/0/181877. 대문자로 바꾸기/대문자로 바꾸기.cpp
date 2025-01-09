dc03dbb65ca3406effe089d697176ec944fb33a6
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-01-09 13:52:38 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(auto c:myString)
+    {
+        answer+=toupper(c);
+    }
+    return answer;
+}
