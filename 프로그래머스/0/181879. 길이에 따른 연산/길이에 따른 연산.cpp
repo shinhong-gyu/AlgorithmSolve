4b2d1fb134ad83499fa56b90fa00ced72ab7e0a6
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-01-09 13:55:32 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    if(num_list.size() >= 11)
+    {
+        for(auto n : num_list)
+        {
+            answer+=n;
+        }
+    }
+    else
+    {
+        answer=1;
+        for(auto n : num_list)
+        {
+            answer*=n;
+        }
+    }
+    return answer;
+}
