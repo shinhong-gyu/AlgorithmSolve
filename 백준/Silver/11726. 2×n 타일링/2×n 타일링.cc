6536d4a25ba4b2fe5b886804552bb01cb0671d6a
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-01-10 15:05:44 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	long long dp[1001];
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= N; i++)
+	{
+		dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+	}
+	cout << dp[N] << endl;
+}
