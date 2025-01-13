5c0428d3dfc451457020cad1c4e88afe0964e4d9
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-01-13 16:56:42 +0900

    [Silver II] Title: 나무 자르기, Time: 144 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,39 +1,52 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
-    long long n, m, max = 0;
-    int tree[1000001];
-    
-    cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> tree[i];
-    
-    sort(tree, tree + n);
-    
-    long long low = 0;
-    long long high = tree[n - 1];
-    
-    while(low <= high){ //cut 가능할 때 까지
-        long long sum = 0;
-        long long cut = (low + high) / 2;
-        
-        for(int i = 0; i < n; i++) {
-            if(tree[i] - cut > 0) sum += tree[i] - cut; // cut 하고 남는 게 있다면 가져감
-        }
-        
-        if(sum >= m){ // m미터보다 가져간 나무가 같거나 많으면
-            max = cut; // 현재 cut 지점을 최대 지점으로 저장
-            low = cut + 1; // cut 가능 구간을 더 올림
-        } else{
-            high = cut - 1; // m미터가 안 되면 cut 가능 구간을 내림
-        }
-    }
-    
-    cout << max; // 최대 cut 지점 출력
-    
-    return 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long N, M;
+	cin >> N >> M;
+
+	vector<int> treeHeight(N);
+	long long highest = -1;
+
+	long long low = 0;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> treeHeight[i];
+		if (treeHeight[i] > highest) highest = treeHeight[i];
+	}
+
+	long long mid = 0, total = 0;
+	long long max = 0;
+	while (highest >= low)
+	{
+		mid = (low + highest) / 2;
+		total = 0;
+
+		for (auto h : treeHeight)
+		{
+			if (h - mid > 0)
+			{
+				total += (h - mid);
+			}
+		}
+		if (total >= M)
+		{
+			max = mid;
+			low = mid + 1;
+		}
+		else
+		{
+			highest = mid - 1;
+		}
+	}
+
+	cout << max << "\n";
 }
