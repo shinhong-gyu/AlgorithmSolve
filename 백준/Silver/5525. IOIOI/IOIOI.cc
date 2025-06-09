62524f4933d761a3982aca29fb86c666f621bcb4
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-06-09 22:44:52 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,47 +1,58 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <string> // std::string 사용을 위해 포함
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int main() {
+    // 입출력 속도 향상
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int N, M;
-	cin >> N;
-	cin >> M;
+    int N, M;
+    cin >> N;
+    cin >> M;
+    string S; 
+    cin >> S;
 
-	string pn = "";
+    int answer = 0;
+    int count_oi = 0; 
 
-	for (int i = 0; i < N; i++)
-	{
-		pn += "IO";
-	}
+ 
+    for (int i = 0; i < M; ++i) 
+    {
+        if (S[i] == 'I') 
+        {
+            if (i + 2 < M && S[i+1] == 'O' && S[i+2] == 'I') 
+            {
+                count_oi++; 
+                i++;
+            } else 
+                {
+                if (count_oi >= N) 
+                {
+                    answer += (count_oi - N + 1);
+                }
+                count_oi = 0;
+            }
+        } 
+        else 
+        { 
+            if (count_oi >= N) 
+            {
+                answer += (count_oi - N + 1);
+            }
+            count_oi = 0;
+        }
+    }
 
-	pn += "I";
 
-	string input;
-	cin >> input;
+    if (count_oi >= N) 
+    {
+        answer += (count_oi - N + 1);
+    }
 
-	int count = 0;
-	string::size_type pos = 0;
+    cout << answer << endl;
 
-
-	while (pos < input.size())
-	{
-		if (input.find(pn, pos) != string::npos)
-		{
-			count++;
-			pos = input.find(pn, pos) + 1;
-		}
-		else
-		{
-			break;
-		}
-	}
-
-	cout << count;
+    return 0;
 }
