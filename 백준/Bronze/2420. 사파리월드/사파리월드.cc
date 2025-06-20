bd4d7d8e9c629cb8994b9b9925c081f0daf62832
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-06-20 18:50:47 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int N,M;
+    cin>>N >> M;
+    
+    
+    
+    cout << (N > M? (long long)N-M : (long long)M-N);
+    
+}    
