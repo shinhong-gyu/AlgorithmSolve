7c4496bc701e126c2bbf8e4a74deee0096a6519f
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-08-19 17:15:28 +0900

    [Gold III] Title: 양팔저울, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+bool dp[55501];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	vector<int> weight(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> weight[i];
+	}
+
+	dp[weight[0]] = true;
+
+	for (int i = 1; i < N; i++)
+	{
+		unordered_set<int> canCheck;
+		for (int k = 1; k < 55001; k++)
+		{
+			if (dp[k])
+			{
+				canCheck.insert(k + weight[i]);
+				canCheck.insert(abs(k - weight[i]));
+				canCheck.insert(weight[i]);
+			}
+		}
+
+		for (int e : canCheck)
+		{
+			dp[e] = true;
+		}
+	}
+
+
+	int M;
+	cin >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		int cur;
+		cin >> cur;
+
+		if (dp[cur])
+		{
+			cout << "Y\n";
+		}
+		else
+		{
+			cout << "N\n";
+		}
+	}
+}
