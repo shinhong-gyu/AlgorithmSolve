b469a6e70d4ecca64dc17e66aa7ce83281b5b7e8
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-09-01 16:04:56 +0900

    [Gold IV] Title: 자두나무, Time: 0 ms, Memory: 2388 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> // max 함수를 사용하기 위해 추가
+
+using namespace std;
+
+int N, W;
+
+int dp[1001][3][31];
+
+int record[1001];
+
+int solve(int sec, int loc, int m)
+{
+	if (sec > N)
+	{
+		return 0;
+	}
+
+	if (dp[sec][loc][m] != -1)
+	{
+		return dp[sec][loc][m];
+	}
+
+	// 그대로 있는 경우
+	int case1 = solve(sec + 1, loc, m);
+
+	// 움직이는 경우
+	int case2 = -1;
+	if (m < W)
+	{
+		int next = loc == 1 ? 2 : 1;
+		case2 = solve(sec + 1, next, m + 1);
+	}
+
+	int cur = (record[sec] == loc) ? 1 : 0;
+
+	dp[sec][loc][m] = cur + max(case1, case2);
+
+	return dp[sec][loc][m];
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> W;
+
+	for (int i = 0; i <= N; i++)
+	{
+		for (int j = 0; j <= 2; j++)
+		{
+			for (int k = 0; k <= W; k++)
+			{
+				dp[i][j][k] = -1;
+			}
+		}
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> record[i];
+	}
+    
+    // 1번 위치에서 시작하는 경우
+    int result1 = solve(1, 1, 0);
+
+    // 2번 위치에서 시작하는 경우 (이동 횟수 1 소모)
+    int result2 = 0;
+    if (W > 0) 
+    {
+        result2 = solve(1, 2, 1);
+    }
+    
+	cout << max(result1, result2);
+}
