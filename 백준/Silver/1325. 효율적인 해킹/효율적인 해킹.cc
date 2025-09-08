0a932e3f6007956942ff1b30d70af8516d11e3dc
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-09-08 17:38:27 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2608 ms, Memory: 3468 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -5,74 +5,61 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+// BFS 함수: start_node에서 해킹할 수 있는 컴퓨터의 수를 반환
+int bfs(int start_node, int N, const vector<vector<int>>& graph) {
+    int count = 1;
+    vector<bool> visited(N + 1, false);
+    queue<int> q;
 
-	int N, M;
-	cin >> N >> M;
+    visited[start_node] = true;
+    q.push(start_node);
 
-	vector<vector<int>> trust(N + 1, vector<int>());
+    while (!q.empty()) {
+        int current_node = q.front();
+        q.pop();
 
+        for (int neighbor : graph[current_node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+                count++;
+            }
+        }
+    }
+    return count;
+}
 
-	for (int i = 0; i < M; i++)
-	{
-		int A, B;
-		cin >> A >> B;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-		trust[A].push_back(B);
-	}
+    int N, M;
+    cin >> N >> M;
 
-	int efficient = -1;
+    // 그래프를 역방향으로 구성
+    vector<vector<int>> trust(N + 1);
+    for (int i = 0; i < M; i++) {
+        int A, B;
+        cin >> A >> B;
+        trust[B].push_back(A); // B를 해킹하면 A를 해킹할 수 있다.
+    }
 
+    int max_count = 0;
+    vector<int> result(N + 1, 0);
 
-	vector<int> answer(N + 1, 0);
+    for (int i = 1; i <= N; i++) {
+        result[i] = bfs(i, N, trust);
+        if (result[i] > max_count) {
+            max_count = result[i];
+        }
+    }
 
-	vector<bool> visited(N + 1, false);
+    for (int i = 1; i <= N; i++) {
+        if (result[i] == max_count) {
+            cout << i << " ";
+        }
+    }
 
-	for (int i = 1; i <= N; i++)
-	{
-		fill(visited.begin(), visited.end(), false);
-
-		if (trust[i].size() <= 0)
-		{
-			answer[i]++;
-			continue;
-		}
-
-
-		queue<int> q;
-
-		q.push(i);
-		visited[i] = true;
-
-		while (!q.empty())
-		{
-			int c = q.front();
-
-			q.pop();
-
-			for (int n : trust[c])
-			{
-				if (!visited[n])
-				{
-					q.push(n);
-					answer[n]++;
-					visited[n] = true;
-				}
-			}
-		}
-	}
-
-	int maxEfficient = *max_element(answer.begin(), answer.end());
-
-	for (int i = 1; i <= N; i++)
-	{
-		if (answer[i] == maxEfficient)
-		{
-			cout << i << " ";
-		}
-	}
+    return 0;
 }
