e5721f02a4a7f2fdd2ea42725a35d7a72b88c784
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-09-23 14:09:56 +0900

    [Gold III] Title: 웜홀, Time: 36 ms, Memory: 2332 KB -BaekjoonHub

---
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <climits>
+
+#define INF INT_MAX
+
+using namespace std;
+
+typedef tuple<int, int, int> Edge;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int N, M, W;
+
+		cin >> N >> M >> W;
+
+		vector<Edge> edges;
+
+		for (int i = 0; i < M; i++)
+		{
+			int a, b, c;
+			cin >> a >> b >> c;
+
+			edges.push_back(make_tuple(a, b, c));
+			edges.push_back(make_tuple(b, a, c));
+		}
+
+
+		bool update = false;
+
+		for (int i = 0; i < W; i++)
+		{
+			int a, b, c;
+			cin >> a >> b >> c;
+
+			edges.push_back(make_tuple(a, b, -1 * c));
+
+			if (c != 0 && a == b)
+			{
+				update = true;
+			}
+		}
+
+		vector<int> D(N + 1, INF);
+
+		for (int i = 0; i < N - 1; i++)
+		{
+			for (auto edge : edges)
+			{
+				int s = get<0>(edge);
+				int e = get<1>(edge);
+				int c = get<2>(edge);
+
+				if (D[s] == INF)
+				{
+					D[e] = c;
+				}
+				else
+				{
+					D[e] = min(D[e], D[s] + c);
+				}
+			}
+		}
+
+		for (int i = 0; i < N - 1; i++)
+		{
+			for (auto edge : edges)
+			{
+				int s = get<0>(edge);
+				int e = get<1>(edge);
+				int c = get<2>(edge);
+
+				if (D[s] != INF)
+				{
+					if (D[e] > D[s] + c)
+					{
+						update = true;
+						break;
+					}
+				}
+			}
+			if (update)
+			{
+				break;
+			}
+		}
+
+		if (update)
+		{
+			cout << "YES" << "\n";
+		}
+		else
+		{
+			cout << "NO" << "\n";
+		}
+	}
+}
