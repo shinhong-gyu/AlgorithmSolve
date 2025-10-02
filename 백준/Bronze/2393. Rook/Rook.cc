ec29139b309b6af6bc61eb577bfcb63d1f898c3c
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-10-02 22:05:08 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+#define ull unsigned long long 
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "  ___  ___  ___\n";
+	cout << "  | |__| |__| |\n";
+	cout << "  |           |\n";
+	cout << "   \\_________/\n";
+	cout << "    \\_______/\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |_____|\n";
+	cout << "  __/       \\__\n";
+	cout << " /             \\\n";
+	cout << "/_______________\\\n";
+
+}
