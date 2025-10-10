6adf3a50cb9c2519a2a43d3ac612ff36ab8fc522
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-10-10 22:21:07 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    for(int i = 0; i<5; i++)
+    {
+        int temp;
+        cin >> temp;
+        sum+= temp;
+    }
+    cout << sum;
+}
