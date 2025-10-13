0512afa4dd20cdc139031febfc982d7f8ceb6903
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-10-13 16:01:44 +0900

    [Gold V] Title: 줄어드는 수, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+#define ull unsigned long long
+
+int N;
+string cur = "";
+ull maxDecs = 9876543210;
+ull answer;
+
+vector<ull> decs;
+
+void solve()
+{
+	if (cur.length() >= 1)
+	{
+		if (stoull(cur) > maxDecs)
+		{
+			return;
+		}
+
+		for (int i = 0; i <= 9; i++)
+		{
+			if (cur.back() - '0' > i)
+			{
+				cur += to_string(i);
+				solve();
+				decs.push_back(stoull(cur));
+				cur.pop_back();
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i <= 9; i++)
+		{
+			cur += to_string(i);
+			solve();
+			decs.push_back(stoull(cur));
+			cur.pop_back();
+		}
+	}
+
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	solve();
+
+	sort(decs.begin(), decs.end());
+
+	if (N > decs.size())
+	{
+		cout << -1;
+	}
+	else
+	{
+		cout << decs[N - 1];
+	}
+}
